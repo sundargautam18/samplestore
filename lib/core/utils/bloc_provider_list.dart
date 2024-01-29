7a3521f7f106878a7e6ff5381c0ee6ed76aaea88
sahilramjali <sahil.ramjali13@gmail.com>
2024-01-29 15:28:45 +0545

    setup

---
+++ lib/core/utils/bloc_provider_list.dart
@@ -0,0 +1,9 @@
+// ignore: depend_on_referenced_packages
+// import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/single_child_widget.dart';
+
+List<SingleChildWidget> get blocProviderList {
+  return [
+    // BlocProvider(create: (context)=>exampleCubit());
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:samplestore/core/config/app_bloc_observer_config.dart';
 import 'package:samplestore/core/router/app_router.dart';
+// import 'package:samplestore/core/utils/bloc_provider_list.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = AppBlocObserver();
+  // runApp(
+  //   MultiBlocProvider(
+  //     providers: blocProviderList,
+  //     child: const MyApp(),
+  //   ),
+  // );
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: appRouter,
+      title: 'Sample Store',
     );
   }
 }
