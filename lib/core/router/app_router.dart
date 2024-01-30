0a4127945c1dddf5d50ad2b855e12dfb966f9b42
sahilramjali <sahil.ramjali13@gmail.com>
2024-01-30 09:15:28 +0545

    setup

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:samplestore/features/home/presentation/ui/home_screen.dart';
 import 'package:samplestore/features/login/presentation/login_screen.dart';
 
 GoRouter appRouter = GoRouter(
   routes: <RouteBase>[
     GoRoute(
-        path: '/',
+        path: '/login',
         builder: (BuildContext context, GoRouterState state) {
           return const LoginScreen();
         }),
+    GoRoute(
+        path: "/",
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomeScreen();
+        })
   ],
 );
