6d2966cdba7d70393fe33ff9d4b8f7e574cfdf44
sundargautam2055 <sundar.gautam@aitc.ai>
2024-02-06 16:59:20 +0545

    local notiification

--- lib/core/messaging_service.dart
+++ lib/core/messaging_service.dart
@@ -14,15 +14,35 @@ class MessagingService {
       if (message.notification != null) {
         print("Notification title: ${message.notification!.title}");
         print("Notification body: ${message.notification!.body}");
-          // show a notification at top of screen.
-    showSimpleNotification(
-        Text(message.notification!.title.toString()),
-        background: Colors.green);
+        // show a notification at top of screen.
+        showSimpleNotification(Text(message.notification!.title.toString()),
+            background: Colors.green);
       }
     });
 
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print("onMessageOpenedApp: $message");
+      print("onMessage: $message");
+      if (message.notification != null) {
+        print("Notification title: ${message.notification!.title}");
+        print("Notification body: ${message.notification!.body}");
+        // show a notification at top of screen.
+        showSimpleNotification(Text(message.notification!.title.toString()),
+            background: Colors.green);
+      }
     });
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  }
+
+  Future<void> _firebaseMessagingBackgroundHandler(
+      RemoteMessage message) async {
+    print("onMessage: $message");
+    if (message.notification != null) {
+      print("Notification title: ${message.notification!.title}");
+      print("Notification body: ${message.notification!.body}");
+      // show a notification at top of screen.
+      showSimpleNotification(Text(message.notification!.title.toString()),
+          background: Colors.green);
+    }
   }
 }
